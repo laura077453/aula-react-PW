4cbc960ce2417cc16c24f6a9d5895064db388d75
laura077453 <laurafreitasdecampos@gmail.com>
2026-09-16 12:12:02 +0000

    projeto base

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div id='container'>
+        <h1> Sejam bem vindos ao react!</h1>
+    </div>
+  )
+}
+
